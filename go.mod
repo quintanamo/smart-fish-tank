377b39f39e0bcd4d1841021b766fc76a12f38f4f
Quintanamo <qherb@ycp.edu>
2021-03-27 17:02:03 -0400

    Connect to the database when the program runs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module quintinherb/smart-fish-tank
 
 go 1.16
+
+require github.com/go-sql-driver/mysql v1.5.0 // indirect
--- smart-fish-tank.go
+++ smart-fish-tank.go
@@ -5,22 +5,50 @@ import (
     "log"
     "net/http"
     "time"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
-func AddCurrentTemperature() {
-	ticker := time.NewTicker(1 * time.Minute)
+// function called when the ticker elapses
+func AddCurrentTemperature(getCurrentTemperatureInterval time.Duration) {
+	ticker := time.NewTicker(getCurrentTemperatureInterval)
 	for _ = range ticker.C {
 		fmt.Println("Tock")
 	}
 }
 
 func main() {
-    go AddCurrentTemperature()
+    // connect to mysql database
+    db, err := sql.Open("mysql", "root:Ycas2018!@tcp(127.0.0.1:3306)/")
+    // if there is an error opening the connection, handle it
+    if err != nil {
+        panic(err.Error())
+    } else {
+        fmt.Println("Successfully connected to MySQL.")
+    }
+    // close db when main finishes executing
+    defer db.Close()
 
+    insert, err := db.Query("CREATE DATABASE IF NOT EXISTS `smart-fish-tank`;")
+    // if there is an error inserting, handle it
+    if err != nil {
+        fmt.Println(err.Error())
+        fmt.Println("Continuing without database \"smart-fish-tank\".")
+    } else {
+        fmt.Println("Using database \"smart-fish-tank\".")
+    }
+    // be careful deferring Queries if you are using transactions
+    defer insert.Close()
+
+    // set defined temperature request interval
+    getCurrentTemperatureInterval := 10 * time.Minute
+    go AddCurrentTemperature(getCurrentTemperatureInterval)
+
+    // serve the static folder
     fileServer := http.FileServer(http.Dir("./static"))
     http.Handle("/", fileServer)
-
-    fmt.Printf("Starting server at port 8080\n")
+    // serve content on port 8080
+    fmt.Printf("Starting server on port 8080\n")
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal(err)
     }
